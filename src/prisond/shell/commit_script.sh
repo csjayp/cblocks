96e6b6d4e8aa688ea37413430f9e8f7d6d2e4cad
Christian S.J. Peron <csjp@freebsd.org>
2020-05-10 21:29:57 -0500

    Disable compression on commit

--- src/prisond/shell/commit_script.sh
+++ src/prisond/shell/commit_script.sh
@@ -5,7 +5,7 @@ set -x
 
 tar -C "${1}/${2}" --exclude="/tmp" \
     --exclude="/dev" \
-    -zcf "${3}/images/${4}.tar.gz" .
+    -cf "${3}/images/${4}.tar.gz" .
 devfs rule -s 5000 delset
 for i in `jot $5 0`; do
     umount "${1}/${i}/dev"
