ac69595ece3b802980a475cc3b99a4b142fcf8ea
Christian S.J. Peron <csjayp@gmail.com>
2025-11-12 08:11:13 +0000

    fix branch name

--- ci/build.sh
+++ ci/build.sh
@@ -6,7 +6,7 @@ export PATH="/usr/local/go125/bin:$PATH"
 export ASSUME_ALWAYS_YES=YES
 
 pkg install go125 git
-go install github.com/golangci/golangci-lint/cmd/golangci-lint@lates
+go install github.com/golangci/golangci-lint/cmd/golangci-lint@latest
 make
 make test
 make lint
