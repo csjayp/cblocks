75f9f59976b882a1a82c689419d7d7f0d3530bdc
Christian S.J. Peron <csjayp@gmail.com>
2025-11-12 07:59:29 +0000

    Install golangci-lint

--- ci/build.sh
+++ ci/build.sh
@@ -6,6 +6,7 @@ export PATH="/usr/local/go125/bin:$PATH"
 export ASSUME_ALWAYS_YES=YES
 
 pkg install go125
+go install github.com/golangci/golangci-lint/cmd/golangci-lint@lates
 make
 make test
 make lint
