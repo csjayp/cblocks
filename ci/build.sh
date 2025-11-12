a1cb95e123996bab4fc8eddcf840bbf514daa3fa
Christian S.J. Peron <csjayp@gmail.com>
2025-11-12 08:05:17 +0000

    install git

--- ci/build.sh
+++ ci/build.sh
@@ -5,7 +5,7 @@ set -e
 export PATH="/usr/local/go125/bin:$PATH"
 export ASSUME_ALWAYS_YES=YES
 
-pkg install go125
+pkg install go125 git
 go install github.com/golangci/golangci-lint/cmd/golangci-lint@lates
 make
 make test
